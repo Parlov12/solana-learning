9e59bfa2211a9e5df58cc2a19ea1da6f45347f54
Petar Parlov - Dell Laptop <petarparlov4@gmail.com>
2025-12-04 10:57:44 +0100

    ownership

--- season-8/lesson-2-Rust/ownership/src/main.rs
+++ season-8/lesson-2-Rust/ownership/src/main.rs
@@ -9,16 +9,49 @@ fn main() {
     // println!("{s}"); -> this would cause error
 
     {
-        let s = String::from("Hello");
+        let mut s = String::from("Hello");
         
         s.push_str(", world!");
 
-        println("{s}");
+        print!("{s}");
     } // Rust calls drop function that clears memory allocated by String s
 
     {
-        let s = String::from("hello");
+        let mut s = String::from("hello");
         s = String::from("New hello"); // Rust automatically call drop function on previous value
     }
 
+    let mut new_string1 = gives_ownership();
+
+    let new_string2 = takes_ownership_and_gives_back(new_string1);
+
+    let new_string3 = String::from("Hello world 3");
+
+    let get_new_string3 = print_and_return_string_reference(&new_string3);
+
+    println!("New printing: {get_new_string3}");
+
+}
+
+// method that gives ownership
+fn gives_ownership() -> String {
+
+    let some_string = String::from("new string");
+
+    some_string
+}
+
+// function that takes ownership
+fn takes_ownership_and_gives_back(str: String) -> String {
+    // ownership is moved into new_string
+    let new_string = str;
+
+    // new_string is returned thus ownership will be moved somewhere else
+    new_string
+}
+
+// reference
+fn print_and_return_string_reference(s: &String) -> &String {
+    println!("{s}");
+    s
 }
